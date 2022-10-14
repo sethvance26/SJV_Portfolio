a15d338323fa39d21dab9447c55ca93543d0f5f0
Seth <sethvance26@gmail.com>
2022-10-14 00:08:19 -0500

    adding bootstrap icons

--- sjv-site/src/components/Navbar.js
+++ sjv-site/src/components/Navbar.js
@@ -1,14 +1,19 @@
 import React from "react";
 import logo from "../assets/logo.svg";
 
+
 const Navbar = () => {
+
+  
   return (
     <>
       <nav className="navbar">
         <div className="container-fluid">
-
         <a href="https://www.google.com/">
           <img src={logo} className="logo" alt="logo" />
+        </a>
+        <a>
+          <i class="bi bi-list" style={{ fontSize: 40 }}></i>
         </a>
         </div>
       </nav>
